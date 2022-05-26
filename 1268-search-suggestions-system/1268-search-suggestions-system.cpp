b493c441f8b94d1c9801a0c0727c056a48b62862
NIHAL AHMAD <nehalhmd14@gmail.com>
2022-05-27 00:03:32 +0530

    Time: 27 ms (99.85%), Space: 23.6 MB (96.23%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -2,26 +2,18 @@ class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
    
-    vector<vector<string>> answer;
-        vector<string> temp;
-        string current = "";
-        sort(products.begin(),products.end());
-        for(auto ch:searchWord)
-        {
-            current+=ch;
-                       temp.clear();
-            int n=current.size();
-          for(auto x:products)
-            {
-           
-                if(x.substr(0,n)==current) 
-                    temp.push_back(x);
-              if(temp.size()==3) break;
-            
-            }
-            answer.push_back(temp);
- 
+     vector<vector<string>> ans;
+        sort(products.begin(), products.end());
+        string key;
+        for(auto c: searchWord){
+            key += c;
+            auto l=lower_bound(products.begin(), products.end(), key);
+            auto r=upper_bound(products.begin(), products.end(), key+='~');
+            key.pop_back();
+            if(l == r) break;
+            ans.emplace_back(l, min(l+3, r));            
         }
-        return answer;
+        while(ans.size() != searchWord.size()) ans.push_back({});
+        return ans; 
     }
 };
