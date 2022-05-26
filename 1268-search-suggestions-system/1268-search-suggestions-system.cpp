426e5402f3a73b9394197d8e1e8390be1cf498c9
NIHAL AHMAD <nehalhmd14@gmail.com>
2022-05-26 23:44:46 +0530

    Time: 1620 ms (5.64%), Space: 331.5 MB (13.66%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,41 +1,27 @@
-struct Trie {
-  Trie(): nodes(26) {}
-  ~Trie() { 
-    for (auto* node : nodes)
-      delete node;
-  }
-  vector<Trie*> nodes;
-  vector<const string*> words;  
-  
-  static void addWord(Trie* root, const string& word) {    
-    for (char c : word) {      
-      if (root->nodes[c - 'a'] == nullptr) root->nodes[c - 'a'] = new Trie();
-      root = root->nodes[c - 'a'];
-      if (root->words.size() < 3)
-        root->words.push_back(&word);
-    }
-  }
-  
-  static vector<vector<string>> getWords(Trie* root, const string& prefix) {
-    vector<vector<string>> ans(prefix.size());
-    for (int i = 0; i < prefix.size(); ++i) {
-      char c = prefix[i];
-      root = root->nodes[c - 'a'];
-      if (root == nullptr) break;
-      for (auto* word : root->words)
-        ans[i].push_back(*word);
-    }
-    return ans;
-  }
-};
- 
 class Solution {
 public:
-  vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-    std::sort(begin(products), end(products));
-    Trie root;
-    for (const auto& product : products)
-      Trie::addWord(&root, product);
-    return Trie::getWords(&root, searchWord);
-  }
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+   
+    vector<vector<string>> answer;
+        vector<string> temp;
+        string current = "";
+        sort(products.begin(),products.end());
+        for(auto ch:searchWord)
+        {
+            current+=ch;
+                       temp.clear();
+            int n=current.size();
+          for(auto x:products)
+            {
+           
+                if(x.substr(0,n)==current) 
+                    temp.push_back(x);
+              if(temp.size()==3) break;
+            
+            }
+            answer.push_back(temp);
+ 
+        }
+        return answer;
+    }
 };
